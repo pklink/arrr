ce9649541d7a9bd0bdf8caf488e7dad82e5081b6
Pierre Klink <dev@klinks.info>
2013-02-25 14:27:49 +0100

    enabled receiver-config

--- config/receiver.php
+++ config/receiver.php
@@ -5,10 +5,10 @@ return [
     'debug'    => false,
     'webroot'  => __DIR__ . '/..',   // optionally, Arrr will use the directory path of this file
     'receiver' => [
-        'enabled'      => false, // optionally; true is the default value
+        'enabled'      => true, // optionally; true is the default value
         'factory'      => ['\Arrr\Service\Receiver\Factory', 'create'], // optionally; this is the default value
-        //'routingParam' => 'nSgBwtF2d6MgjH5pRc',
-        'routingParam' => null, // required if 'enabled' is true; string with minimum length of 12 characters
+        'routingParam' => 'nSgBwtF2d6MgjH5pRc',
+        //'routingParam' => null, // required if 'enabled' is true; string with minimum length of 12 characters
         // TODO: implement process
         'process'  => 'Git', // optionally; this is the default value
         'priority' => 100,   // optionally; this is the default value
